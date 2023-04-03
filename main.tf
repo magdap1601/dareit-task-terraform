0fd9930dc9fa22e632648dd500f119df65a455e3
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-03 20:58:08 +0200

    fix: public access prevention bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "google_storage_bucket" "static-site" {
   name          = "my-bucket-by-terraform_pull_req"
   location      = "EU"
 }
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+
+  public_access_prevention = "enforced"
+}
