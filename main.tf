da50c6d61ccaa418defc8eba3ec0b58b667e6cfa
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-03 19:40:04 +0200

    feat: create new bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "dareit-vm-ci" {
+//resource "google_compute_instance" "dareit-vm-ci" {
   name         = "dareit-vm-tf-ci"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
@@ -21,4 +21,8 @@ resource "google_compute_instance" "dareit-vm-ci" {
       // Ephemeral public IP
     }
   }
+}//
+resource "google_storage_bucket" "bucket" {
+ name   = "bucket-created-on-newBranch"
+ location = "US"
 }
