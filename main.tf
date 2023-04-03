94c32dec7ed75c945ea0b4bc2b4f196d0f8f69b7
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-03 21:04:00 +0200

    fix: update main

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 resource "google_storage_bucket" "static-site" {
   name          = "my-bucket-by-terraform_pull_req"
   location      = "EU"
-}
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
-  location      = "US"
-
-  public_access_prevention = "enforced"
+public_access_prevention = "enforced"
 }
