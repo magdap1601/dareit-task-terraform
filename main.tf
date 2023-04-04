7e79b8f9f5a28eabbd8279147549a6df603d84b9
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-04 13:39:46 +0200

    feat: add bucekt on pull request

--- main.tf
+++ main.tf
@@ -1,24 +1,6 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+resource "google_storage_bucket" "auto-expire" {
+  name     = "no-public-access-bucket"
+  location = "US"
 
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+  public_access_prevention = "enforced"
 }
