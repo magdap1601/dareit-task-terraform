3dc8acb0187f58ecce970c66f53e7bd62521820d
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-04 09:31:41 +0200

    fix: add bucket and instance

--- main.tf
+++ main.tf
@@ -1,6 +1,31 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
-  location      = "US"
+resource "google_compute_instance" "dareit-vm-ci" {
+  name         = "dareit-vm-tf-ci"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
-  public_access_prevention = "enforced"
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+ name   = "bucket-created-by-terraform_pull_req"
+ location = "US"
+
+ public_access_prevention = "enforced"
 }
