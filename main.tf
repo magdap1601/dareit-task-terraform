7f0224417b0a0dbfcafa08b606e6b0316b7dba18
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-04 13:53:49 +0200

    feat: create new branch with new bucket without prevent public access option

--- main.tf
+++ main.tf
@@ -1,24 +1,5 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+resource "google_storage_bucket" "bucket" {
+  name     = "my-bucket2"
+  location = "US"
 
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
 }
